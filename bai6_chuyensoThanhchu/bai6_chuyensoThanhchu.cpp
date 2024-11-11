6a1b73ec89af21d531aad072698dd5da44d86919
vothanhnhan <vothanhnhan22824@gmail.com>
2024-11-11 11:24:02 +0700

    Add project files.

---
+++ bai6_chuyensoThanhchu/bai6_chuyensoThanhchu.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    cout << "UNG DUNG GIAI PHUONG BAC 2\n"; 
+    double a, b, c, delta, x1, x2;
+    cout << "Nhap a:"; cin >> a;
+    cout << "Nhap b:"; cin >> b;
+    cout << "Nhap c:"; cin >> c;
+    if (a == 0){
+        // giai pt bac 1:bx + c=0
+        if (a == 0)
+        {
+            cout << "phuong trinh co nghiem duy nhat x=" << -c / b << endl;
+    }
+        else {
+            if (c != 0)
+        }
+    else{
+        cout<<phuong trinh vo so nghiem\n
